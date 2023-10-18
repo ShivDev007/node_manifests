1e717b01b8722006ea83c6474c7bd277034521d0
ShivDev007 <shivamsharma2695@gmail.com>
2023-10-18 19:04:08 +0530

    this is added to edit the conn.js where i applied env vars

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,19 +1,43 @@
-const mysql = require("mysql2");
+// const mysql = require("mysql2");
 
+
+// // Create a connection to the database
+// const connection = mysql.createConnection({
+//   host: "database-1.c55mts1blx53.ap-south-1.rds.amazonaws.com",
+//   user: "root",
+//   password: "redhat3439",
+//   port: "3306",
+//   database: "employees_db"
+// });
+
+// // open the MySQL connection
+// connection.connect(error => {
+//   if (error) throw error;
+//   console.log("Successfully connected to the MYSQL database.");
+// });
+
+// module.exports = connection;
+
+
+// different part 
+const mysql = require("mysql2");
+const dotenv = require("dotenv");
+
+dotenv.config(); // Load environment variables from .env
 
 // Create a connection to the database
 const connection = mysql.createConnection({
-  host: "database-1.c55mts1blx53.ap-south-1.rds.amazonaws.com",
-  user: "root",
-  password: "redhat3439",
-  port: "3306",
-  database: "employees_db"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME
 });
 
-// open the MySQL connection
+// Open the MySQL connection
 connection.connect(error => {
   if (error) throw error;
-  console.log("Successfully connected to the MYSQL database.");
+  console.log("Successfully connected to the MySQL database.");
 });
 
 module.exports = connection;
